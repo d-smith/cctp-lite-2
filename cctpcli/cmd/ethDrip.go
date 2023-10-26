d59d102da5de023a0817dc4b3a59b5af9306a8ff
d-smith <doug.smith.mail@gmail.com>
2023-10-26 10:40:36 -0700

    Added eth fiddy faucet

---
+++ cctpcli/cmd/ethDrip.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"cctpcli/eth"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/spf13/cobra"
+)
+
+// ethDripCmd represents the ethDrip command
+var ethDripCmd = &cobra.Command{
+	Use:   "ethDrip [account] [amount]",
+	Short: "Drip some Fiddy to an account",
+	Long:  `Drip some Fiddy to an account"`,
+	Args:  cobra.MinimumNArgs(2),
+	Run:   dripEthCmd,
+}
+
+func init() {
+	rootCmd.AddCommand(ethDripCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// ethDripCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// ethDripCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func dripEthCmd(cmd *cobra.Command, args []string) {
+	if len(args) != 2 {
+		fmt.Println("ethDrip requires exactly two arguments")
+		return
+	}
+	amount, ok := new(big.Int).SetString(args[1], 10)
+	if !ok {
+		fmt.Println("ethDrip requires a valid amount")
+		return
+	}
+
+	dripEth(args[0], amount)
+}
+
+func dripEth(address string, amount *big.Int) {
+	ethContext := eth.NewEthereumContext()
+
+	txnid, err := ethContext.Drip(address, amount)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Dripped %s to %s: txn id %s\n", amount.String(), address, txnid)
+}
--- cctpcli/eth/eth.go
+++ cctpcli/eth/eth.go
@@ -4,12 +4,16 @@ import (
 	"cctpcli/conn"
 	"cctpcli/fiddy"
 	"context"
+	"crypto/ecdsa"
+	"errors"
+	"fmt"
 	"log"
 	"math/big"
 	"os"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -17,10 +21,21 @@ type EthereumContext struct {
 	client          *ethclient.Client
 	ethFiddy        *fiddy.Fiddy
 	ethFiddyAddress string
+	dripKey         *ecdsa.PrivateKey
 }
 
 func NewEthereumContext() *EthereumContext {
 	ethClient := conn.GetEthClient(conn.ETHEREUM)
+
+	dripKeyFromEnv := os.Getenv("FIDDY_ETH_DRIP_KEY")
+	if dripKeyFromEnv == "" {
+		log.Fatal("FIDDY_ETH_DRIP_KEY not set")
+	}
+
+	dripKey, err := crypto.HexToECDSA(dripKeyFromEnv)
+	if err != nil {
+		log.Fatal("Error processing key from env", err)
+	}
 
 	ethFiddyAddress := os.Getenv("FIDDY_ETH_ADDRESS")
 	if ethFiddyAddress == "" {
@@ -32,12 +47,25 @@ func NewEthereumContext() *EthereumContext {
 		log.Fatal(err)
 	}
 
-	return &EthereumContext{client: ethClient, ethFiddy: ethFiddy,
-		ethFiddyAddress: ethFiddyAddress}
+	return &EthereumContext{client: ethClient,
+		ethFiddy:        ethFiddy,
+		ethFiddyAddress: ethFiddyAddress,
+		dripKey:         dripKey,
+	}
 }
 
 func NewMBEthereumContext() *EthereumContext {
 	ethClient := conn.GetEthClient(conn.MOONBEAM)
+
+	dripKeyFromEnv := os.Getenv("FIDDY_MB_DRIP_KEY")
+	if dripKeyFromEnv == "" {
+		log.Fatal("FIDDY_MB_DRIP_KEY not set")
+	}
+
+	dripKey, err := crypto.HexToECDSA(dripKeyFromEnv)
+	if err != nil {
+		log.Fatal("Error processing key from env", err)
+	}
 
 	ethFiddyAddress := os.Getenv("FIDDY_MB_ADDRESS")
 	if ethFiddyAddress == "" {
@@ -49,8 +77,11 @@ func NewMBEthereumContext() *EthereumContext {
 		log.Fatal(err)
 	}
 
-	return &EthereumContext{client: ethClient, ethFiddy: ethFiddy,
-		ethFiddyAddress: ethFiddyAddress}
+	return &EthereumContext{client: ethClient,
+		ethFiddy:        ethFiddy,
+		ethFiddyAddress: ethFiddyAddress,
+		dripKey:         dripKey,
+	}
 }
 
 func (ec *EthereumContext) GetBalance(address string) (*big.Int, error) {
@@ -67,4 +98,37 @@ func (ec *EthereumContext) GetBalance(address string) (*big.Int, error) {
 func (ec *EthereumContext) GetFiddyBalance(address string) (*big.Int, error) {
 	addressForBalance := common.HexToAddress(address)
 	return ec.ethFiddy.BalanceOf(&bind.CallOpts{}, addressForBalance)
+}
+
+func (ec *EthereumContext) Drip(address string, amount *big.Int) (string, error) {
+	publicKey := ec.dripKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return "", errors.New("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	nonce, err := ec.client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println("nonce", nonce)
+
+	gasPrice, err := ec.client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return "", err
+	}
+
+	auth := bind.NewKeyedTransactor(ec.dripKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)     // in wei
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	toAddress := common.HexToAddress(address)
+
+	tx, err := ec.ethFiddy.Transfer(auth, toAddress, amount)
+	return tx.Hash().Hex(), err
 }
