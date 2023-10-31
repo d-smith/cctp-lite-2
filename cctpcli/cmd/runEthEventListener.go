81836d8655ea1e419e75e55265ae77e724bd9e7b
d-smith <doug.smith.mail@gmail.com>
2023-10-31 13:22:02 -0700

    Added decoding of message sent payload

--- cctpcli/cmd/runEthEventListener.go
+++ cctpcli/cmd/runEthEventListener.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"crypto/ecdsa"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"os"
@@ -14,6 +15,7 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/holiman/uint256"
 	"github.com/spf13/cobra"
 )
 
@@ -97,6 +99,13 @@ func listener(cmd *cobra.Command, args []string) {
 
 			fmt.Printf("export MSG=%s\n", hexutil.Encode(ms.Message))
 
+			parsedMessage, err := parseMessageSent(ms.Message)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			printMessage(parsedMessage)
+
 		}
 	}
 }
@@ -110,4 +119,125 @@ func personalSign(message string, privateKey *ecdsa.PrivateKey) (string, error) 
 	}
 	signatureBytes[64] += 27
 	return hexutil.Encode(signatureBytes), nil
+}
+
+type MessageSent struct {
+	// local domain - uint32
+	// remote domain - uint32
+	// nonce - uint64
+	// sender - bytes32
+	// recipient - bytes32
+	// burn message - bytes
+	MessageVersion uint32
+	LocalDomain    uint32
+	RemoteDomain   uint32
+	Nonce          uint64
+	Sender         string
+	Recipient      string
+	BurnMessage    *BurnMessage
+}
+
+func parseMessageSent(messageSent []byte) (*MessageSent, error) {
+	if len(messageSent) < 84 {
+		return nil, fmt.Errorf("invalid message sent length")
+	}
+
+	messageVersion := binary.BigEndian.Uint32(messageSent[0:4])
+	localDomain := binary.BigEndian.Uint32(messageSent[4:8])
+	remoteDomain := binary.BigEndian.Uint32(messageSent[8:12])
+	nonce := binary.BigEndian.Uint64(messageSent[12:20])
+
+	sender := messageSent[20:52]
+	senderHex := hexutil.Encode(sender[12:32])
+
+	recipient := messageSent[52:84]
+	recipientHex := hexutil.Encode(recipient[12:32])
+
+	burnMessage := messageSent[84:]
+
+	parsedBurnMessage, err := parseBurnMessage(burnMessage)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MessageSent{
+		MessageVersion: messageVersion,
+		LocalDomain:    localDomain,
+		RemoteDomain:   remoteDomain,
+		Nonce:          nonce,
+		Sender:         senderHex,
+		Recipient:      recipientHex,
+		BurnMessage:    parsedBurnMessage,
+	}, nil
+}
+
+type BurnMessage struct {
+	Version       uint32
+	BurnToken     string
+	MintRecipient string
+	Amount        *uint256.Int
+	Sender        string
+}
+
+func parseBurnMessage(burnMessage []byte) (*BurnMessage, error) {
+	if len(burnMessage) < 132 {
+		return nil, fmt.Errorf("invalid burn message length")
+	}
+
+	burnMessageVersion := binary.BigEndian.Uint32(burnMessage[0:4])
+	burnToken := burnMessage[4:36]
+	mintRecipient := burnMessage[36:68]
+
+	amountBytes := burnMessage[68:100]
+	hexAmount := hexutil.Encode(amountBytes[12:32])
+
+	// Convert hexAmount to a uint256
+
+	i := 0
+	if hexAmount[0:2] == "0x" {
+		i = 2
+	}
+
+	for ; i < len(hexAmount); i++ {
+		if hexAmount[i] != '0' {
+			break
+		}
+	}
+
+	if i == len(hexAmount) {
+		hexAmount = "0x0"
+	} else {
+		hexAmount = fmt.Sprintf("0x%s", hexAmount[i:])
+	}
+
+	amountDec, err := uint256.FromHex(hexAmount)
+	if err != nil {
+		return nil, err
+	}
+
+	sender := burnMessage[100:132]
+
+	return &BurnMessage{
+		Version:       burnMessageVersion,
+		BurnToken:     hexutil.Encode(burnToken[12:32]),
+		MintRecipient: hexutil.Encode(mintRecipient[12:32]),
+		Amount:        amountDec,
+		Sender:        hexutil.Encode(sender[12:32]),
+	}, nil
+}
+
+func printMessage(m *MessageSent) {
+	fmt.Printf("Message fields\n")
+	fmt.Printf("Message Version: %d\n", m.MessageVersion)
+	fmt.Printf("Local domain: %d\n", m.LocalDomain)
+	fmt.Printf("Remote domain: %d\n", m.RemoteDomain)
+	fmt.Printf("Nonce: %d\n", m.Nonce)
+	fmt.Printf("Sender: %s\n", m.Sender)
+	fmt.Printf("Recipient: %s\n", m.Recipient)
+	fmt.Printf("Burn message:\n")
+	fmt.Printf("  Version: %d\n", m.BurnMessage.Version)
+	fmt.Printf("  Burn token: %s\n", m.BurnMessage.BurnToken)
+	fmt.Printf("  Mint recipient: %s\n", m.BurnMessage.MintRecipient)
+	fmt.Printf("  Amount: %d\n", m.BurnMessage.Amount)
+	fmt.Printf("  Sender: %s\n", m.BurnMessage.Sender)
 }
---
+++ scratch/go.mod
@@ -0,0 +1,8 @@
+module scratch
+
+go 1.20
+
+require (
+	github.com/ethereum/go-ethereum v1.13.4
+	github.com/holiman/uint256 v1.2.3
+)
